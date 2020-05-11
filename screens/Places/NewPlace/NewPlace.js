f80a7375539b3c0709711c836d1a473be5270d1e
Diogo Silva <diogosilva@MBP-de-Diogo.lan>
2020-05-11 11:22:13 +0100

    Simple input to add places and add redux package

--- screens/Places/NewPlace/NewPlace.js
+++ screens/Places/NewPlace/NewPlace.js
@@ -1,16 +1,47 @@
 // React
-import React from 'react';
+import React, { useState } from 'react';
 import {
     StyleSheet,
     View,
+    TextInput,
+    Text,
+    ScrollView,
+    Button
 } from 'react-native';
+
+// Constants
+import Colors from '../../../constants/styles/Colors';
+
 
 // Component
 const NewPlace = () => {
+    const [title, setTitle] = useState('');
+
+    const titleChangeHandler = text => {
+        // Logic to validate
+        setTitle(text);
+    };
+
+    const savePlace = () => {
+
+    };
+
     return (
-        <View>
-            
-        </View>
+        <ScrollView>
+            <View style={styles.form}>
+                <Text style={styles.label}>Title</Text>
+                <TextInput
+                    style={styles.input}
+                    value={title}
+                    onChangeText={titleChangeHandler}
+                />
+                <Button
+                    title="Save Place"
+                    color={Colors.primary}
+                    onPress={savePlace}
+                />
+            </View>
+        </ScrollView>
     );
 };
 
@@ -20,6 +51,21 @@ NewPlace.navigationOptions = {
 };
 
 // Styles
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    form: {
+        margin: 30
+    },
+    label: {
+        fontSize: 18,
+        marginBottom: 15
+    },
+    input: {
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+        marginBottom: 15,
+        paddingVertical: 4,
+        paddingHorizontal: 2
+    }
+});
 
 export default NewPlace;
